844ee38bb4c97a3e9ec52287d501b190b7a41059
yasinyasa <yasinyasacontact@gmail.com>
2023-01-26 19:38:18 +0300

    task 4 yapıldı

--- src/Gun01/Okul.java
+++ src/Gun01/Okul.java
@@ -3,6 +3,6 @@ package Gun01;
 public class Okul {
     public static void main(String[] args) {
 
-        System.out.println("okuldan merhaba");
+        System.out.println("okuldanmerhaba");
     }
 }
